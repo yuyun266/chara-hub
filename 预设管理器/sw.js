c9fe8e757c6185fb22bb8a731e2f28ac75e7ccb2
yuyun266 <dt9f64k8mm@privaterelay.appleid.com>
2026-02-14 20:24:06 +0800

    Add files via upload

---
+++ 预设管理器/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'chara-hub-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
